45d541da1ad6f1eb5e55a25bb807149668d31383
AmaraU <amaraude01@gmail.com>
2025-09-25 01:25:02 +0100

    back to og

--- src/services/index.js
+++ src/services/index.js
@@ -1,3 +1,2 @@
-// export const BaseUrl = process.env.API_BASE_URL;
-// export const BaseUrl = import.meta.env.VITE_API_BASE_URL;
-export const BaseUrl = '/proapi/';
+export const BaseUrl = import.meta.env.VITE_API_BASE_URL;
+// export const BaseUrl = '/proapi/';
